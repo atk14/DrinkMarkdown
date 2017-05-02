e60363c4bdd5f33741e064e793c4e25654318e55
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-05-02 16:11:09 +0200

    Syntax highlighting rewritten

--- src/drink_markdown_prefilter.php
+++ src/drink_markdown_prefilter.php
@@ -2,55 +2,59 @@
 class DrinkMarkdownPrefilter {
 
 	function filter($raw){
-		$out = array();
 		$GLOBALS["wiki_replaces"] = array();
 
 		$raw = "\n$raw\n";
 		
 		$replaces = array();
-
 		$uniqid = uniqid();
 
-		$raw = preg_replace_callback('/[\n\r]```([ a-z0-9]*)[\n\r](.*?)\n```[\n\r]/s','_drink_markdown_replace_source',$raw);
+		// Source codes wrapped in ```...```
+		preg_match_all('/[\n\r]```([ a-z0-9]*)[\n\r](.*?)\n```[\n\r]/s',$raw,$matches);
+		for($i=0;$i<sizeof($matches[0]);$i++){
+			$snippet = $matches[0][$i];
+			$source = $this->formatSourceCode($matches[2][$i],array("lang" => $matches[1][$i]));
+			$placeholder = "source.$i.$uniqid";
+			$replaces[$snippet] = "\n\n$placeholder\n\n";
 
+			$GLOBALS["wiki_replaces"]["<p>$placeholder</p>"] = $source;
+		}
+
+		// HTML tables
 		preg_match_all('/\n<table\b[^>]*>.*?<\/table>\s*?\n/si',$raw,$matches);
 		for($i=0;$i<sizeof($matches[0]);$i++){
 			$snippet = $matches[0][$i];
 			$table = trim($snippet);
-			$replacement = "table.$i.$uniqid";
-			$replaces[$snippet] = "\n\n$replacement\n\n";
+			$placeholder = "table.$i.$uniqid";
+			$replaces[$snippet] = "\n\n$placeholder\n\n";
 			
-			$GLOBALS["wiki_replaces"]["<p>$replacement</p>"] = $table;
+			$GLOBALS["wiki_replaces"]["<p>$placeholder</p>"] = $table;
 		}
 
-		//var_dump($GLOBALS["wiki_replaces"]); exit;
-
-		$raw = strtr($raw,$replaces);
-
-		//echo $raw; exit;
+		$raw = EasyReplace($raw,$replaces);
 
 		return $raw;
 	}
-}
 
-function _drink_markdown_replace_source($matches){
-	($lang = trim($matches[1])); // "php", "sql", "auto"
-	$source = trim($matches[2]);
+	/**
+	 * $source = $this->formatSourceCode($raw_source,array("lang" => "php"));
+	 */
+	function formatSourceCode($source,$options = array()){
+		$options += array(
+			"lang" => ""
+		);
 
-	$id = "drinkreplace.".uniqid();
+		if(strlen($options["lang"])){
+			$geshi = new GeSHi($source, $options["lang"]);
+			$geshi->enable_keyword_links(false);
+			$geshi->set_overall_style("");
+			$geshi->enable_classes(false);
+			$source = $geshi->parse_code();
 
-	if(strlen($lang)){
-		$geshi = new GeSHi($source, $lang);
-		$geshi->enable_keyword_links(false);
-		$geshi->set_overall_style("");
-		$geshi->enable_classes(false);
-		$source = $geshi->parse_code();
-
-		$source = preg_replace('/^<pre class="[^"]+"/','<pre',$source); // '<pre class="javascript">' -> '<pre>'
-	}else{
-		$source = '<pre><code>'.htmlentities($source).'</code></pre>';
+			$source = preg_replace('/^<pre class="[^"]+"/','<pre',$source); // '<pre class="javascript">' -> '<pre>'
+		}else{
+			$source = '<pre><code>'.htmlentities($source).'</code></pre>';
+		}
+		return $source;
 	}
-
-	$GLOBALS["wiki_replaces"]["<p>$id</p>"] = $source;
-	return "\n\n$id\n\n";
 }
