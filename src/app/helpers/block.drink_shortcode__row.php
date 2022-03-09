504256be749f220f7ef9603643a00b47e08ba504
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-03-09 22:32:39 +0100

    Counting columns fixed in the shortcode row

--- src/app/helpers/block.drink_shortcode__row.php
+++ src/app/helpers/block.drink_shortcode__row.php
@@ -13,7 +13,7 @@ function smarty_block_drink_shortcode__row($params,$content,$template,&$repeat){
 	// counting columns
 	$_content = trim($content);
 	$_content = preg_replace('/<!-- drink:row -->.*?<!-- \/drink:row -->/s','',$_content);
-	preg_match_all('/(!-- drink:col -->)/',trim($_content),$matches);
+	preg_match_all('/(!-- drink:col .*?-->)/',trim($_content),$matches); // e.g. !-- drink:col class="alert-success" -->
 	$number_of_columns = isset($matches[1]) && sizeof($matches[1]) ? sizeof($matches[1]) : 2;
 
 	$template->assign("content",$content);
